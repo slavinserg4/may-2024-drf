15adf7e90e60589b901fa79a75b0b43c7f854e16
slavinserg4 <slavinserg4@gmail.com>
2024-12-13 15:36:37 +0200

    hw pizza

---
+++ pizza/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from pizza.models import PizzaModel
+
+
+class PizzaSerializer(serializers.Serializer):
+    Id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    price = serializers.IntegerField()
+    size = serializers.FloatField()
+
+    def create(self, validated_data:dict):
+        return PizzaModel.objects.create(**validated_data)
+    def update(self, instance, validated_data:dict):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        return instance
